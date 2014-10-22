1b11ab607f70192c72c9c68d53517f0aaf28b43f
Azul <mail@azulinho.com>
2014-10-22 22:13:26 +0100

    adding test

---
+++ tests/DSL-tutorial-test.groovy
@@ -0,0 +1,12 @@
+job {
+    name 'DSL-Tutorial-1-Test'
+    scm {
+        git('git://github.com/jgritman/aws-sdk-test.git')
+    }
+    triggers {
+        scm('*/15 * * * *')
+    }
+    steps {
+        maven('-e clean test')
+    }
+}
