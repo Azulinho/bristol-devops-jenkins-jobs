48d7bb6a1116f266486fe92d36b05796990fb4b9
Azul <mail@azulinho.com>
2014-10-24 21:48:34 +0100

    trying downstreams

--- jobs/DEPLOY/DEPLOY-job1.groovy
+++ jobs/DEPLOY/DEPLOY-job1.groovy
@@ -1,10 +1,17 @@
 job {
     name 'DEPLOY-job1'
+    parameters {
+        stringParam("PARAM1")
+    }
     steps {
+        shell("echo DEPLOY 1")
+    }
+    publishers {
         downstreamParameterized {
             trigger('DEPLOY-job2') {
                 currentBuild()
             }
         }
+
     }
 }
