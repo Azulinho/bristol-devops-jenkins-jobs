d5020e96b9452cb4919b63e11ae33035d451b09c
Azul <mail@azulinho.com>
2014-10-24 21:08:09 +0100

    trying out fixes in git plugin

--- jobs/BUILD/BUILD-job1.groovy
+++ jobs/BUILD/BUILD-job1.groovy
@@ -2,6 +2,11 @@ job {
     name 'BUILD-job1'
     scm {
         git('https://github.com/Azulinho/bristol-devops-jenkins-jobs.git')
+        { node ->
+            node / authorOrCommitter << 'true'
+            node / gitConfigName << 'Azul'
+            node / gitConfigEmail << 'notme@gmail.org'
+        }
     }
     triggers {
         scm('*/15 * * * *')
