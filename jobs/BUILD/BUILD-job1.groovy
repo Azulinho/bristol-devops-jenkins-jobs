cdb8342d18243bf2a779c0e4a81daab38a2f1fec
Azul <mail@azulinho.com>
2014-10-24 21:40:30 +0100

    move to publisher

--- jobs/BUILD/BUILD-job1.groovy
+++ jobs/BUILD/BUILD-job1.groovy
@@ -2,16 +2,14 @@ job {
     name 'BUILD-job1'
     scm {
         git('https://github.com/Azulinho/bristol-devops-jenkins-jobs.git')
-        { node ->
-            node / authorOrCommitter << 'true'
-            node / gitConfigName << 'Azul'
-            node / gitConfigEmail << 'notme@gmail.org'
-        }
     }
     triggers {
         scm('*/15 * * * *')
     }
     steps {
+        shell('echo hello')
+    }
+    publishers {
         downstreamParameterized {
             trigger('DEPLOY-job1') {
                 currentBuild()
