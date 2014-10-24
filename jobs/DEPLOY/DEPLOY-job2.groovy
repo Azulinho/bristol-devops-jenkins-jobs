7dd63c93e8641a4e644ce1a12a5f8ce147b8e07a
Azul <mail@azulinho.com>
2014-10-24 22:00:30 +0100

    lxi

--- jobs/DEPLOY/DEPLOY-job2.groovy
+++ jobs/DEPLOY/DEPLOY-job2.groovy
@@ -1,6 +1,9 @@
 job {
     name 'DEPLOY-job2'
     steps {
+        shell("echo lixo")
+    }
+    publishers {
         downstreamParameterized {
             trigger('DEPLOY-job3') {
                 currentBuild()
