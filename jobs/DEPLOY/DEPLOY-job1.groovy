4b132eb84956d6acc7ba90c34fd56646be118a69
Azul <mail@azulinho.com>
2014-10-30 23:46:28 +0000

    delivery pipeline

--- jobs/DEPLOY/DEPLOY-job1.groovy
+++ jobs/DEPLOY/DEPLOY-job1.groovy
@@ -2,7 +2,10 @@ job {
     name 'DEPLOY-job1'
     parameters {
         stringParam("PARAM1")
+        stringParam("PARAM1")
     }
+    deliveryPipelineConfiguration("Stage 1", "Task 1")
+
     steps {
         shell("echo DEPLOY 1")
     }
--- jobs/DEPLOY/DEPLOY-job2.groovy
+++ jobs/DEPLOY/DEPLOY-job2.groovy
@@ -3,6 +3,7 @@ job {
     steps {
         shell("echo lixo")
     }
+    deliveryPipelineConfiguration("Stage 1", "Task 2")
     publishers {
         downstreamParameterized {
             trigger('DEPLOY-job3') {
