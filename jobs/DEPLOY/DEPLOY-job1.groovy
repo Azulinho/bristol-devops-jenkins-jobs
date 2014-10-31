212a0c937f85aaa78a0bd91ab16af75a4b809ec4
Azul <mail@azulinho.com>
2014-10-31 00:45:59 +0000

    tidy up

--- jobs/DEPLOY/DEPLOY-job1.groovy
+++ jobs/DEPLOY/DEPLOY-job1.groovy
@@ -11,13 +11,16 @@ job {
         (it / 'blockBuildWhenDownstreamBuilding').setValue('true')
     }
 
-    configure {
-        (it / 'properties' / 'hudson.plugins.throttleconcurrents.ThrottleJobProperty' / 'maxConcurrentPerNode').setValue('0')
-        (it / 'properties' / 'hudson.plugins.throttleconcurrents.ThrottleJobProperty' / 'maxConcurrentTotal').setValue('1')
-        (it / 'properties' / 'hudson.plugins.throttleconcurrents.ThrottleJobProperty' / 'throttleEnabled').setValue(true)
-        (it / 'properties' / 'hudson.plugins.throttleconcurrents.ThrottleJobProperty' / 'throttleOption').setValue('category')
-        (it / 'properties' / 'hudson.plugins.throttleconcurrents.ThrottleJobProperty' / 'categories').setValue('Stage 1')
+    configure { project ->
+        project / properties << 'hudson.plugins.throttleconcurrents.ThrottleJobProperty' {
+i           maxConcurrentPerNode    '0'
+            maxConcurrentTotal      '1'
+            throttleEnabled         'true'
+            throttleOption          'category'
+            categories              'Stage 1'
+        }
     }
+
 
     steps {
         shell("echo DEPLOY 1")
